ef5e502537dff6ff572d49b7c60733e7fe02d90e
Philipp Schröer <hebipp1@googlemail.com>
2012-02-21 18:05:09 +0100

    Now using Maven

---
+++ src/main/java/de/philworld/bukkit/compassex/CompassEx.java
@@ -0,0 +1,63 @@
+package de.philworld.bukkit.compassex;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CompassEx extends JavaPlugin {
+
+    Logger log = Logger.getLogger("Minecraft"); 
+    FileConfiguration config;
+    
+    // save all hidden players in a list
+    List<String> hiddenPlayers = new ArrayList<String>();
+    
+    HashMap<String, Location> deathPoints = new HashMap<String, Location>();
+    
+    
+    @Override
+    public void onEnable() {
+        loadConfiguration();
+        
+        PluginManager pm = getServer().getPluginManager();
+        
+        // set up listener
+        pm.registerEvents(new CompassExListener(this), this);
+       
+        // setup compass tracker
+        CompassTrackerUpdater.setPlugin(this);
+        CompassTrackerUpdater.setUpdateRate((long) getConfig().getInt("live-update-rate"));
+        
+        // set command executor
+        getCommand("compass").setExecutor(new CompassExCommandExecutor(this));
+        
+        // done.
+        PluginDescriptionFile pff = this.getDescription();
+        log.info(pff.getName() +  " " + pff.getVersion() + " is enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+    	CompassTrackerUpdater.stop(); // stop tasks
+    	
+        PluginDescriptionFile pff = this.getDescription();
+        log.info(pff.getName() +  " " + pff.getVersion() + " is disabled.");
+    }
+
+    /**
+     * Loads the configuration and inserts the defaults.
+     */
+    public void loadConfiguration() {
+        config = getConfig();
+        config.options().copyDefaults(true);
+        saveConfig();
+    }
+
+}
